942a4a997a6607e824fc3140449c66d9a3a93826
sina-Kim <sinabero3271@gmail.com>
2022-03-09 00:58:48 +0900

    레이아웃 잡기
    Header 레이아웃 (Nav처럼 양옆에 고정)
    Footer 바닥 고정

--- src/App.svelte
+++ src/App.svelte
@@ -2,4 +2,56 @@
   let name = "world";
 </script>
 
-<h1>Hello {name}!</h1>
+<header>
+  <div class="flex-container">
+    <div
+      style="width: 50px; height: 50px; border: 1px solid;"
+      class="flex-item"
+    />
+    <div
+      style="width: 50px; height: 50px; border: 1px solid;"
+      class="flex-item"
+    />
+    <div class="flex-right-item" />
+    <div
+      style="width: 50px; height: 50px; border: 1px solid;"
+      class="flex-item"
+    />
+    <div
+      style="width: 50px; height: 50px; border: 1px solid;"
+      class="flex-item"
+    />
+  </div>
+</header>
+<main />
+<footer>footer</footer>
+
+<style>
+  /* flex: https://d2.naver.com/helloworld/8540176#ch4 */
+  header {
+    min-height: 20px;
+    background-color: antiquewhite;
+  }
+
+  footer {
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    min-height: 20px;
+    background-color: antiquewhite;
+  }
+
+  .flex-container {
+    display: flex;
+  }
+
+  .flex-item {
+    background-color: aqua;
+    flex: none;
+  }
+
+  .flex-right-item {
+    background-color: aqua;
+    margin-left: auto;
+  }
+</style>
