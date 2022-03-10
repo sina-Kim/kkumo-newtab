f7992fbfbcd018f75e6b1bb468baaf081314bc49
sina-Kim <63644856+sina-Kim@users.noreply.github.com>
2022-03-10 17:59:11 +0900

    App2.svelte 추가
    기능 테스트의 편의성을 위해 별도의 진입	 svelte 파일 추가

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
-import App from './App.svelte';
+/* 
+ * sina-Kim, 진입 파일을 확인해주세요. 
+ * 기능 영역을 확인하기 위해 별도의 진입 파일 App2.svelte를 생성하였습니다.
+*/
+import App from './App2.svelte';
 
 var app = new App({
 	target: document.body
